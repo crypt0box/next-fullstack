a5e245013647957bd2915b50dec00cd001c0e442
cryptobox <pray.happy76@gmail.com>
2024-08-17 16:26:58 +0900

    feat: ログイン機能の実装

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,7 +8,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
-    //Google({})
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
   pages: {
     signIn: "/login",
