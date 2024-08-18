fa0ffb0033d09603886b695cfbf7b140a6b60031
cryptobox <pray.happy76@gmail.com>
2024-08-18 21:31:19 +0900

    feat; 記事投稿ページを仮で作成

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,37 @@
-export default function DashboardPage() {
+import DashboardHeader from "@/components/dashboard-header";
+import DashboardShell from "@/components/dashboard-shell";
+import PostCreateButton from "./post-create-button";
+import { db } from "@/lib/db";
+import { getCurrentUser } from "@/lib/session";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
+  const posts = await db.post.findMany({
+    where: {
+      authorId: user.id,
+    },
+    select: {
+      id: true,
+      title: true,
+      published: true,
+      createdAt: true,
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+  });
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <DashboardShell>
+      <DashboardHeader heading="記事投稿" text="記事の投稿と管理">
+        <PostCreateButton />
+      </DashboardHeader>
+    </DashboardShell>
   );
 }
---
+++ app/(dashboard)/dashboard/post-create-button.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Icon } from "@/components/icon";
+import { ButtonProps, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+interface PostCreateButtonProps extends ButtonProps {}
+
+export default function PostCreateButton({
+  className,
+  variant,
+  ...props
+}: PostCreateButtonProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {};
+
+  return (
+    <button
+      className={cn(
+        buttonVariants({ variant }),
+        {
+          "cursor-not-allowed opacity-60": isLoading,
+        },
+        className
+      )}
+      onClick={onClick}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? (
+        <Icon.spinner className="animate-spin mr-2 h-4 w-4" />
+      ) : (
+        <Icon.add className="mr-2 h-4 w-4" />
+      )}
+      新しい投稿
+    </button>
+  );
+}
