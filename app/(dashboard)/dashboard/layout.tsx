41aa9ef879d9e54b9ea347d8a3cc46df194596e0
cryptobox <pray.happy76@gmail.com>
2024-08-18 10:23:51 +0900

    feat: サイドバーを作成

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import DashboardNav from "@/components/dashboard-nav";
 import MainNav from "@/components/main-nav";
 import SiteFooter from "@/components/ui/site-footer";
 import { dashboardConfig } from "@/config/dashboard";
@@ -8,12 +9,20 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <header>
-        <div>
+    <div className="flex min-h-screen flex-col space-y-6">
+      <header className="sticky top-0 z-40 border-b bg-background">
+        <div className="container flex items-center justify-between py-4 h-16">
           <MainNav items={dashboardConfig.mainNav} />
         </div>
       </header>
+      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+        <aside className="hidden md:flex w-[200px] flex-col">
+          <DashboardNav items={dashboardConfig.sidebarNav} />
+        </aside>
+        <main className="flex flex-col w-full flex-1 overflow-hidden">
+          {children}
+        </main>
+      </div>
       <SiteFooter />
     </div>
   );
